68321c0fe787a03c1ba51956c56142b328798a49
hebersadurni <arsehase@live.com.mx>
2019-05-11 05:05:39 -0400

    TicTacToe Last tests

--- app/src/main/java/com/example/tictactoejava/MainActivity.java
+++ app/src/main/java/com/example/tictactoejava/MainActivity.java
@@ -15,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
 
 
     Integer LINE_SIZE = 3;
+    String PLAYER_PIECE="X";
     String[] board = new String[LINE_SIZE * LINE_SIZE];
-    String[] screenBoard = new String[LINE_SIZE * LINE_SIZE];
+    Integer[] playerBoard = new Integer[LINE_SIZE * LINE_SIZE];
+    Integer[] computerBoard = new Integer[LINE_SIZE * LINE_SIZE];
+    Integer[] emptySpots=new Integer[LINE_SIZE];
+    int firstShot=-1;
+    int playerMax=0;
+    int computerMax=0;
+    int posPlayerMax=0;
+    int center=50;
+    int posComputerMax=0;
     Boolean playerTurn = false;
 
     Button[] boardButton = new Button[LINE_SIZE * LINE_SIZE];
@@ -71,6 +80,9 @@ public class MainActivity extends AppCompatActivity {
     void placePiece(View v) {
         v.setOnClickListener(null);
         if (!playerTurn) {
+            if(firstShot==-1){
+                firstShot=v.getId();
+            }
             board[v.getId()] = "X";
             v.setBackgroundColor(getResources().getColor(R.color.colorPlayer1));
             playerTurn = true;
@@ -80,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
         checkCompleteBoard();
         if(gameOver.equals("no")) {
-            computerPlays();
+            computerPlays(v);
             checkCompleteBoard();
         }
 
@@ -133,35 +145,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void checkBoardAi(int depth, int turn) {
-        String checkPosibilities = "Empty";
-        checkPosibilities = checkBoardColumns(checkPosibilities, screenBoard);
-        if (checkPosibilities.equals("Empty")) {
-            checkPosibilities = checkBoardLines(checkPosibilities, screenBoard);
-        }
-        if (checkPosibilities.equals("Empty")) {
-            checkPosibilities = checkBoardDiagonalRight(checkPosibilities, screenBoard);
-        }
-        if (checkPosibilities.equals("Empty")) {
-            checkPosibilities = checkBoardDiagonalLeft(checkPosibilities, screenBoard);
-        }
-        if (checkPosibilities.equals("Empty")) {
-            checkPosibilities = checkForTie(checkPosibilities, screenBoard);
-        }
-        if (!checkPosibilities.equals("Empty")) {
-            if (checkPosibilities.equals("X")) {
-                depth++;
-                cleanEmptyAIBoard();
-                minMax(depth, COMPUTER_TURN);
-            } else if(checkPosibilities.equals("O") || checkPosibilities.equals("Tie")) {
-                boardButton[nextAiMove].setBackgroundColor(getResources().getColor(R.color.colorPlayer2));
-                boardButton[nextAiMove].setOnClickListener(null);
-                board[nextAiMove] = "O";
-                playerTurn = false;
-            }
-            checkPosibilities = "Empty";
-        }
-    }
 
     String checkForTie(String playerWinner, String[] boardCheck) {
         int counter = 0;
@@ -252,6 +235,7 @@ public class MainActivity extends AppCompatActivity {
 
     void cleanBoardComplete() {
         gameOver="no";
+        firstShot=0;
         for (int clean = 0; clean < LINE_SIZE * LINE_SIZE; clean++) {
             board[clean] = "0";
             playerTurn = false;
@@ -265,55 +249,335 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    void computerPlays() {
-        minMax(DEPTH, COMPUTER_TURN);
+    void computerPlays(View v) {
+        minMax();
+        int aiMove=checkAiMove();
+        board[aiMove] = "O";
+        boardButton[aiMove].setBackgroundColor(getResources().getColor(R.color.colorPlayer2));
+        boardButton[aiMove].setOnClickListener(null);
+        playerTurn = false;
     }
 
     void cleanEmptyAIBoard() {
         for (int clean = 0; clean < LINE_SIZE * LINE_SIZE; clean++) {
-            screenBoard[clean] = board[clean];
+            playerBoard[clean] = 0;
+            computerBoard[clean] = 0;
         }
+        playerMax=0;
+        computerMax=0;
         startedFilling = false;
         fillEmptyBoard = 0;
     }
 
-    void minMax(int depth, int turn) {
-
-        if (depth >= LINE_SIZE * LINE_SIZE) {
-            depth = 0;
+    void cleanEmptySpots(){
+        for(int clean=0;clean<LINE_SIZE;clean++){
+            emptySpots[clean]=0;
         }
+    }
 
-        for (int completeBoard = depth; completeBoard < LINE_SIZE * LINE_SIZE; completeBoard++) {
-            if (turn == 0 && screenBoard[completeBoard].equals("0")) {
-                screenBoard[completeBoard] = "X";
-            } else if (turn == 1 && screenBoard[completeBoard].equals("0")) {
-                if (!startedFilling) {
-                    nextAiMove = completeBoard;
-                    startedFilling = true;
+    void minMax() {
+
+        int howManyX=0;
+        int howManyO=0;
+        int spaces=0;
+        int score=0;
+        int score2=0;
+        center=50;
+        boolean lineiSNotEmpty=false;
+
+        //CheckLines
+        for(int checkLinesAi=0; checkLinesAi<LINE_SIZE*LINE_SIZE;checkLinesAi+=LINE_SIZE) {
+            for (int checkArray = checkLinesAi; checkArray < LINE_SIZE + checkLinesAi; checkArray++) {
+
+                if (board[checkArray].equals("X")) {
+                    howManyX++;
+                    lineiSNotEmpty = true;
+                } else if (board[checkArray].equals("O")) {
+                    howManyO++;
+                    lineiSNotEmpty = true;
+                } else {
+                    emptySpots[spaces] = checkArray;
+                    spaces++;
                 }
-                screenBoard[completeBoard] = "O";
             }
-            if(turn==1)
-            {
-                turn=0;
-            }else{
-                turn=1;
+
+            if (!lineiSNotEmpty) {
+                cleanEmptySpots();
+            }
+            lineiSNotEmpty = false;
+
+
+            for (int checkPieces = 0; checkPieces < spaces; checkPieces++) {
+                if (emptySpots[checkPieces] != 0 && emptySpots[checkPieces] % 2 != 0 && emptySpots[checkPieces] != LINE_SIZE + 1) {
+                    score = center * howManyX;
+                    score2 = center * howManyO;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                } else if (emptySpots[checkPieces] == LINE_SIZE + 1) {
+                    score = 90 * howManyX;
+                    score2 = 90 * howManyO;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                } else if (emptySpots[checkPieces] == 0 || emptySpots[checkPieces] % 2 == 0) {
+                    score = 75 * howManyX;
+                    score2 = 75 * howManyO;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                }
+            }
+            spaces = 0;
+            howManyO = 0;
+            howManyX = 0;
+        }
+        //CheckColumns
+        for(int checkColumnsAi=0; checkColumnsAi<LINE_SIZE;checkColumnsAi++) {
+            for (int checkArray = checkColumnsAi; checkArray < LINE_SIZE + (checkColumnsAi + LINE_SIZE +1); checkArray += LINE_SIZE) {
+                if (board[checkArray].equals("X")) {
+                    howManyX++;
+                    lineiSNotEmpty = true;
+                } else if (board[checkArray].equals("O")) {
+                    howManyO++;
+                    lineiSNotEmpty = true;
+                } else {
+                    emptySpots[spaces] = checkArray;
+                    spaces++;
+                }
+            }
+            if (!lineiSNotEmpty) {
+                cleanEmptySpots();
+            }
+            lineiSNotEmpty = false;
+            for (int checkPieces = 0; checkPieces < spaces; checkPieces++) {
+                if (emptySpots[checkPieces] != 0 && emptySpots[checkPieces] % 2 != 0 && emptySpots[checkPieces] != LINE_SIZE + 1) {
+                    score2 = center * howManyO;
+                    score = center * howManyX;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                } else if (emptySpots[checkPieces] == LINE_SIZE + 1) {
+                    score2 = 90 * howManyO;
+                    score = 90 * howManyX;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                } else if (emptySpots[checkPieces] == 0 || emptySpots[checkPieces] % 2 == 0) {
+                    score2 = 75 * howManyO;
+                    score = 75 * howManyX;
+                    if (playerBoard[emptySpots[checkPieces]] < score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if (computerBoard[emptySpots[checkPieces]] < score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                }
+
+            }
+            howManyO = 0;
+            howManyX = 0;
+            spaces = 0;
+        }
+            cleanEmptySpots();
+
+        //CheckDiagonalRight
+            for (int checkArray = 0; checkArray < (LINE_SIZE*LINE_SIZE); checkArray+=LINE_SIZE+1) {
+
+                if (board[checkArray].equals("X")) {
+                    howManyX++;
+                    lineiSNotEmpty = true;
+                } else if (board[checkArray].equals("O")) {
+                    howManyO++;
+                    lineiSNotEmpty = true;
+                } else {
+                    emptySpots[spaces] = checkArray;
+                    spaces++;
+                }
+            }
+            if (!lineiSNotEmpty) {
+                cleanEmptySpots();
+            }
+            lineiSNotEmpty = false;
+            for (int checkPieces = 0; checkPieces < spaces; checkPieces++) {
+                if(emptySpots[checkPieces]!=0 && emptySpots[checkPieces]%2!=0 && emptySpots[checkPieces]!=LINE_SIZE+1){
+                    score2 =center*howManyO;
+                    score=center*howManyX;
+                    if(playerBoard[emptySpots[checkPieces]]<score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if( computerBoard[emptySpots[checkPieces]]<score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                }else if(emptySpots[checkPieces]==LINE_SIZE+1){
+                    score2=90*howManyO;
+                    score=90*howManyX;
+                    if(playerBoard[emptySpots[checkPieces]]<score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if( computerBoard[emptySpots[checkPieces]]<score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                }else if(emptySpots[checkPieces]==0 || emptySpots[checkPieces]%2==0){
+                    score2=75*howManyO;
+                    score=75*howManyX;
+                    if(playerBoard[emptySpots[checkPieces]]<score) {
+                        playerBoard[emptySpots[checkPieces]] = score;
+                    }
+                    if( computerBoard[emptySpots[checkPieces]]<score2) {
+                        computerBoard[emptySpots[checkPieces]] = score2;
+                    }
+                }
+            }
+        cleanEmptySpots();
+        howManyO=0;
+        howManyX=0;
+        spaces=0;
+
+        //CheckDiagonalLeft
+        for (int checkArray = LINE_SIZE-1; checkArray < (LINE_SIZE*LINE_SIZE)-1; checkArray+=LINE_SIZE-1) {
+            if (board[checkArray].equals("X")) {
+                howManyX++;
+                lineiSNotEmpty = true;
+            } else if (board[checkArray].equals("O")) {
+                howManyO++;
+                lineiSNotEmpty = true;
+            } else {
+                emptySpots[spaces] = checkArray;
+                spaces++;
             }
         }
-        for(int emptySpaces=0; emptySpaces<depth;emptySpaces++){
-            if (turn == 0 && screenBoard[emptySpaces].equals("0")) {
-                screenBoard[emptySpaces] = "X";
-            } else if (turn == 1 && screenBoard[emptySpaces].equals("0")) {
-                screenBoard[emptySpaces] = "O";
-            }
-            if(turn==1)
-            {
-                turn=0;
-            }else{
-                turn=1;
+        if (!lineiSNotEmpty) {
+            cleanEmptySpots();
+        }
+        lineiSNotEmpty = false;
+        for (int checkPieces = 0; checkPieces < spaces; checkPieces++) {
+            if(emptySpots[checkPieces]!=0 && emptySpots[checkPieces]%2!=0 && emptySpots[checkPieces]!=LINE_SIZE+1){
+                score2=center*howManyO;
+                score=center*howManyX;
+                if(playerBoard[emptySpots[checkPieces]]<score) {
+                    playerBoard[emptySpots[checkPieces]] = score;
+                }
+                if( computerBoard[emptySpots[checkPieces]]<score2) {
+                    computerBoard[emptySpots[checkPieces]] = score2;
+                }
+            }else if(emptySpots[checkPieces]==LINE_SIZE+1){
+                score2=90*howManyO;
+                score=90*howManyX;
+                if(playerBoard[emptySpots[checkPieces]]<score) {
+                    playerBoard[emptySpots[checkPieces]] = score;
+                }
+                if( computerBoard[emptySpots[checkPieces]]<score2) {
+                    computerBoard[emptySpots[checkPieces]] = score2;
+                }
+            }else if(emptySpots[checkPieces]==0 || emptySpots[checkPieces]%2==0){
+                score2=75*howManyO;
+                score=75*howManyX;
+                if(playerBoard[emptySpots[checkPieces]]<score) {
+                    playerBoard[emptySpots[checkPieces]] = score;
+                }
+                if( computerBoard[emptySpots[checkPieces]]<score2) {
+                    computerBoard[emptySpots[checkPieces]] = score2;
+                }
             }
         }
-        checkBoardAi(depth, turn);
+        cleanEmptySpots();
+        howManyO=0;
+        howManyX=0;
+        spaces=0;
+
+
+        if((board[LINE_SIZE*LINE_SIZE-2].equals(PLAYER_PIECE) && board[(LINE_SIZE+LINE_SIZE-1)].equals(PLAYER_PIECE))
+                || (board[LINE_SIZE*LINE_SIZE-2].equals(PLAYER_PIECE) && board[LINE_SIZE].equals(PLAYER_PIECE))
+        ){
+            if(board[LINE_SIZE * LINE_SIZE - 1].equals("0")) {
+                playerBoard[LINE_SIZE * LINE_SIZE - 1] = 100;
+            }
+        }
+        if((board[LINE_SIZE].equals(PLAYER_PIECE) && board[LINE_SIZE+1-LINE_SIZE].equals(PLAYER_PIECE))
+                || (board[LINE_SIZE+1-LINE_SIZE].equals(PLAYER_PIECE) && board[LINE_SIZE+LINE_SIZE-1].equals(PLAYER_PIECE))
+        ){
+            if(board[0].equals("0")) {
+                playerBoard[0] = 100;
+            }
+        }
+
+        if(board[LINE_SIZE-1].equals(PLAYER_PIECE) && board[LINE_SIZE*LINE_SIZE-2].equals(PLAYER_PIECE)){
+            if(board[LINE_SIZE*LINE_SIZE-1].equals("0")) {
+                playerBoard[LINE_SIZE * LINE_SIZE - 1] = 100;
+            }
+        }
+        if(board[LINE_SIZE-LINE_SIZE].equals(PLAYER_PIECE) && board[LINE_SIZE*LINE_SIZE-2].equals(PLAYER_PIECE)){
+            if(board[LINE_SIZE].equals("0")) {
+                playerBoard[LINE_SIZE] = 100;
+            }
+        }
+        if(board[LINE_SIZE*(LINE_SIZE-1)].equals(PLAYER_PIECE) && board[LINE_SIZE+(LINE_SIZE-1)].equals(PLAYER_PIECE)){
+            if(board[LINE_SIZE * LINE_SIZE - 1].equals("0")) {
+                playerBoard[LINE_SIZE * LINE_SIZE - 1] = 100;
+            }
+        }
+        //Diagonal Case
+        if(board[LINE_SIZE-1].equals(PLAYER_PIECE) && board[LINE_SIZE*(LINE_SIZE-1)].equals(PLAYER_PIECE)){
+            if(board[LINE_SIZE].equals("0")) {
+                playerBoard[LINE_SIZE] = 100;
+            }
+        }
+        if(board[LINE_SIZE-LINE_SIZE].equals(PLAYER_PIECE) && board[LINE_SIZE*LINE_SIZE-1].equals(PLAYER_PIECE)){
+            if(board[LINE_SIZE].equals("0")) {
+                playerBoard[LINE_SIZE] = 100;
+            }
+        }
+    }
+
+
+
+    int checkAiMove() {
+        int move = 0;
+
+        for(int checkMax=0; checkMax<LINE_SIZE*LINE_SIZE;checkMax++){
+            if(playerBoard[checkMax]>playerBoard[playerMax]){
+                playerMax=checkMax;
+                if(playerBoard[checkMax]>100){
+                    playerBoard[checkMax]=100;
+                }
+            }
+            if(computerBoard[checkMax]>computerBoard[computerMax]){
+                computerMax=checkMax;
+                if(computerBoard[checkMax]>100){
+                    computerBoard[checkMax]=100;
+                }
+            }
+        }
+
+        if(playerBoard[playerMax]>computerBoard[computerMax]){
+            move=playerMax;
+        }else if(computerBoard[computerMax]>=playerBoard[playerMax]){
+            move=computerMax;
+        }
+        for(int removeScores=0;removeScores<LINE_SIZE*LINE_SIZE;removeScores++){
+            playerBoard[removeScores]=0;
+            computerBoard[removeScores]=0;
+        }
+        playerMax=0;
+        computerMax=0;
+
+        return move;
     }
 }
 
